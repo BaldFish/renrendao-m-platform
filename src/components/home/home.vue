75ff407bb0dfb8661dd7cd30c522b3832694d98a
lwc <wence.lv@cnlaunch.com>
2018-11-27 18:14:08 +0800

    开发

---
+++ src/components/home/home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    $END$
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "home",
+    components: {},
+    data() {
+      return {}
+    },
+    created() {
+    },
+    mounted() {
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+  }
+</script>
+
+<style scoped lang="stylus">
+  .home {
+  }
+</style>
