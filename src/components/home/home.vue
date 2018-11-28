44db8627c42797ea67fb98ec0f900f0613b3282e
lwc <wence.lv@cnlaunch.com>
2018-11-28 10:20:19 +0800

    分享页开发完成

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -30,6 +30,22 @@
           </li>
         </ul>
       </div>
+      <div class="about explain" >
+        <h3><img src="./images/explain.png" alt=""></h3>
+        <p>人人道，全球首款集结积分、资产、身份为一体的基于区块链技
+          术的新型移动端应用。</p>
+        <ul class="text">
+          <li>
+            <i></i><span>在这里，您可以一键免费生成您的区块链车证及身份证，生成即可赚钱；</span>
+          </li>
+          <li>
+            <i></i><span>在这里，您是手中数据唯一的主权所有者，一旦上链，尊享永久收益权；</span>
+          </li>
+          <li>
+            <i></i><span>在这里，倚靠区块链技术，将带您进入一个前所未有的新世界。</span>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -108,26 +124,67 @@
       }
       .about{
         padding-top 310px
+        padding-bottom 68px
         h3{
           text-align center
           font-size 0
+          margin-bottom 43px
         }
         .text{
           box-sizing border-box
           margin 0 auto
-          padding 20px
+          padding 40px 50px
           width: 685px;
-          height: 482px;
+          //height: 482px;
           background-color: #ffffff;
           box-shadow: -5px 1px 26px 1px rgba(0, 0, 0, 0.11);
           border-radius: 50px;
           border: solid 1px #eeeeee;/*no*/
           li{
+            font-family: SourceHanSansCN-Regular;
             color #333333
-            line-height 36px;
+            line-height 46px;
             font-size 22px;/*px*/
             span{
+              font-family: SourceHanSansCN-Medium;
               font-size 30px;/*px*/
+            }
+          }
+        }
+      }
+      .explain{
+        padding-top 0
+        h3{
+          margin-bottom 20px
+        }
+        p{
+          width 685px
+          margin 0 auto
+          text-align center
+          font-size: 24px;
+          color: #222222;
+          line-height 32px
+          margin-bottom 40px
+        }
+        .text{
+          li{
+            font-size 0
+            i{
+              display inline-block
+              background-color: #5226f3;
+              width 16px
+              height 16px
+              border-radius 50%
+              vertical-align top
+              margin-top 8px
+              margin-right 18px
+            }
+            span{
+              display inline-block
+              width 524px
+              font-size 22px;/*px*/
+              line-height 30px
+              font-family: SourceHanSansCN-Regular;
             }
           }
         }
